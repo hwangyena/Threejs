61d642591f92094fd97609448440cdf6b2768c9e
SK플래닛 황예나 <hwangyena@sk.com>
2023-02-08 16:03:07 +0900

    CH01: 03.Basic scene

--- ch01/script.js
+++ ch01/script.js
@@ -1,1 +1,33 @@
-console.log('hello world');
+// Scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+// Size
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height); // renderer(canvas) 크기 설정
+renderer.render(scene, camera);
