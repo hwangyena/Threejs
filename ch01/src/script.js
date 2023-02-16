66ca8c7a48fc0796aee7e28e62a66cc3440dcbbd
SK플래닛 황예나 <hwangyena@sk.com>
2023-02-16 13:09:26 +0900

    CH01: 08. Fullscreen and resizing

--- ch01/src/script.js
+++ ch01/src/script.js
@@ -10,40 +10,40 @@ const scene = new THREE.Scene();
 
 // Size
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
 // Objects
-const group = new THREE.Group();
-scene.add(group);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+});
 
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-
-group.add(mesh);
-
-// Axes Helper
-const axesHelper = new THREE.AxesHelper(2); //길이 설정 가능
-scene.add(axesHelper);
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
-  1,
+  0.1,
   1000
 );
-
-// const aspectRatio = sizes.width / sizes.height;
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   -1
-// );
 
 camera.position.z = 3;
 scene.add(camera);
@@ -62,29 +62,12 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setSize(sizes.width, sizes.height); // renderer(canvas) 크기 설정
-renderer.render(scene, camera);
-
-/**
- * Cursor
- */
-const cursor = { x: 0, y: 0 };
-
-window.addEventListener('mousemove', (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = -(event.clientY / sizes.height - 0.5);
-});
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animation
  */
 const loop = () => {
-  // camera.position.x = cursor.x * 10;
-  // camera.position.y = cursor.y * 10;
-  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  // camera.position.y = cursor.y * 5;
-  // camera.lookAt(mesh.position);
-
   renderer.render(scene, camera);
 
   controls.update();
