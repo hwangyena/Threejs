a920ead960939115ffcc9821c70ccbf0e87e8561
SK플래닛 황예나 <hwangyena@sk.com>
2023-02-16 09:59:52 +0900

    CH01: 07. Camera

--- ch01/src/script.js
+++ ch01/src/script.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three';
-import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+/**
+ * Threejs
+ */
 
 // Scene
 const scene = new THREE.Scene();
@@ -14,29 +18,44 @@ const sizes = {
 const group = new THREE.Group();
 scene.add(group);
 
-const cube1 = new THREE.Mesh(
+const mesh = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
   new THREE.MeshBasicMaterial({ color: 0xff0000 })
 );
 
-group.add(cube1);
+group.add(mesh);
 
 // Axes Helper
 const axesHelper = new THREE.AxesHelper(2); //길이 설정 가능
 scene.add(axesHelper);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  1000
+);
 
-camera.position.x = 1;
-camera.position.y = 1;
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1
+// );
+
 camera.position.z = 3;
 scene.add(camera);
 
-// camera.lookAt(new THREE.Vector3(0, 0, 0));
+camera.lookAt(mesh.position);
 
 // Renderer
 const canvas = document.querySelector('.webgl');
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 const renderer = new THREE.WebGLRenderer({
   canvas,
@@ -45,28 +64,32 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height); // renderer(canvas) 크기 설정
 renderer.render(scene, camera);
 
-// Clock
-const clock = new THREE.Clock();
+/**
+ * Cursor
+ */
+const cursor = { x: 0, y: 0 };
 
-// Animation
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+});
+
+/**
+ * Animation
+ */
 const loop = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  // cube1.rotation.y += 0.001 * clock;
-  cube1.rotation.y = elapsedTime;
+  // camera.position.x = cursor.x * 10;
+  // camera.position.y = cursor.y * 10;
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position);
 
   renderer.render(scene, camera);
+
+  controls.update();
 
   window.requestAnimationFrame(loop);
 };
 
 loop();
-
-//////////////////////////////////
-
-// GSAP
-gsap.to(cube1.position, {
-  x: 2,
-  delay: 1,
-  duration: 1,
-});
