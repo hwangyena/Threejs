b51bac8c8c9e128cc2896962a52fa6eb820baaf6
Hwang yena <center7332@naver.com>
2023-03-18 15:42:42 +0900

    [CH02] 20. Scroll based animation

--- ch02/src/script.js
+++ ch02/src/script.js
@@ -1,13 +1,27 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'lil-gui';
+import gsap from 'gsap';
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI();
+
+const parameters = {
+  materialColor: '#E96479',
+};
+
+gui.addColor(parameters, 'materialColor').onChange(() => {
+  material.color.set(parameters.materialColor);
+});
+
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load('/gradients/3.jpg');
+gradientTexture.magFilter = THREE.NearestFilter; // gradient 단계별 섀딩 선택 가능, 없으면 스무스하게 적용됨
 
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI({ width: 300 });
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
@@ -15,202 +29,194 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
- * Galaxy
+ * Object
  */
-const parameters = {
-    count: 10000,
-    size: 0.02,
-    radius: 3,
-    branches: 3,
-    spin: 1,
-    randomness: 0.1,
-    randomnessPower: 3,
-    insideColor: '#ff6030',
-    outsideColor: '#1b3984',
-};
 
-let geometry = null;
-let material = null;
-let points = null;
+const material = new THREE.MeshToonMaterial({
+  color: parameters.materialColor,
+  gradientMap: gradientTexture,
+});
 
-const generateGalaxy = () => {
-    // 이전 내용 삭제
-    if (points !== null) {
-        geometry.dispose(); // 메모리 해제
-        material.dispose();
-        scene.remove(points);
-    }
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
+const mesh3 = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+  material
+);
 
-    // Geometry
-    geometry = new THREE.BufferGeometry();
-    const positions = new Float32Array(parameters.count * 3); // xyz
-    const colors = new Float32Array(parameters.count * 3); // RGB
+scene.add(mesh1, mesh2, mesh3);
 
-    const colorInside = new THREE.Color(parameters.insideColor);
-    const colorOutside = new THREE.Color(parameters.outsideColor);
+const objectsDistance = 4;
 
-    for (let i = 0; i < parameters.count; i++) {
-        const i3 = i * 3;
+mesh1.position.y = -objectsDistance * 0;
+mesh2.position.y = -objectsDistance * 1;
+mesh3.position.y = -objectsDistance * 2;
 
-        // Position
-        const radius = Math.random() * parameters.radius; // 0 ~ 반지름까지 랜덤 값
-        const spinAngle = radius * parameters.spin; // radius에서 멀어질수록 더 큰 spin 필요
-        const branchAngle =
-            ((i % parameters.branches) / parameters.branches) * Math.PI * 2; // 0 ~ 1까지의 값 * 1바퀴 돌때 위치
+mesh1.position.x = 2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
 
-        // 균일하게 퍼져있는 나선형
-        // const [randomX, randomY, randomZ] = [
-        //     (Math.random() - 0.5) * parameters.randomness,
-        //     (Math.random() - 0.5) * parameters.randomness,
-        //     (Math.random() - 0.5) * parameters.randomness,
-        // ];
+const sectionMeshes = [mesh1, mesh2, mesh3];
 
-        // 가운데로 집중되어 있는 나선형
-        const [randomX, randomY, randomZ] = [
-            Math.pow(Math.random(), parameters.randomnessPower) *
-                (Math.random() < 0.5 ? 1 : -1),
-            Math.pow(Math.random(), parameters.randomnessPower) *
-                (Math.random() < 0.5 ? 1 : -1),
-            Math.pow(Math.random(), parameters.randomnessPower) *
-                (Math.random() < 0.5 ? 1 : -1),
-        ];
+/**
+ * Particles
+ */
+const count = 200;
 
-        positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX; //x
-        positions[i3 + 1] = 0 + randomY; //y
-        positions[i3 + 2] =
-            Math.sin(branchAngle + spinAngle) * radius + randomZ; //z
+const positions = new Float32Array(count * 3); //x,y,z를 가지므로
+for (let i = 0; i < count; i++) {
+  positions[i * 3 + 0] = (Math.random() - 0.5) * 10; //x
+  positions[i * 3 + 1] =
+    objectsDistance / 2 -
+    Math.random() * objectsDistance * sectionMeshes.length; //y - 위(현재 화면 distance 1/2)부터 아래(objectDistance)까지
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10; //z
+}
 
-        // Color
-        const mixedColor = colorInside.clone();
-        mixedColor.lerp(colorOutside, radius / parameters.radius); //mixedColor(0) + colorOutside(1) 색상 섞은 정도 (0~1 정도)
+const bufferGeometry = new THREE.BufferGeometry();
+bufferGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3)
+);
 
-        colors[i3] = mixedColor.r;
-        colors[i3 + 1] = mixedColor.g;
-        colors[i3 + 2] = mixedColor.b;
-    }
+const bufferMaterial = new THREE.PointsMaterial({
+  size: 0.02,
+  sizeAttenuation: true, // 크기가 카메라 위치에 따라 변하는지
+});
 
-    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+const buffer = new THREE.Points(bufferGeometry, bufferMaterial);
 
-    // Material
-    material = new THREE.PointsMaterial({
-        size: parameters.size,
-        sizeAttenuation: true,
-        depthWrite: false,
-        blending: THREE.AdditiveBlending,
-        vertexColors: true,
-    });
+scene.add(buffer);
 
-    // Points
-    points = new THREE.Points(geometry, material);
-    scene.add(points);
-};
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
 
-generateGalaxy();
-
-gui.add(parameters, 'count')
-    .min(10)
-    .max(100000)
-    .step(100)
-    .onFinishChange(generateGalaxy);
-gui.add(parameters, 'size')
-    .min(0.001)
-    .max(0.1)
-    .step(0.001)
-    .onFinishChange(generateGalaxy);
-gui.add(parameters, 'radius')
-    .min(0.01)
-    .max(20)
-    .step(0.01)
-    .onFinishChange(generateGalaxy);
-gui.add(parameters, 'branches')
-    .min(2)
-    .max(20)
-    .step(1)
-    .onFinishChange(generateGalaxy);
-gui.add(parameters, 'spin')
-    .min(-5)
-    .max(5)
-    .step(0.01)
-    .onFinishChange(generateGalaxy);
-gui.add(parameters, 'randomness')
-    .min(0)
-    .max(2)
-    .step(0.01)
-    .onFinishChange(generateGalaxy);
-gui.add(parameters, 'randomnessPower')
-    .min(1)
-    .max(10)
-    .step(0.1)
-    .onFinishChange(generateGalaxy);
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy);
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy);
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 /**
  * Camera
  */
+// Group
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.1,
-    100
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
 );
-camera.position.x = 3;
-camera.position.y = 3;
-camera.position.z = 3;
-scene.add(camera);
-
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+camera.position.z = 6;
+cameraGroup.add(camera);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
+  canvas: canvas,
+  alpha: true, //캔버스를 HTML 뒤에 나둘 때 사용 (검정 배경색 없애줌)
 });
+
+// renderer.setClearAlpha(1); //0:흰 ~ 1:검 알파 설정 가능
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * 스크롤
+ */
+
+let currentSection = 0;
+
+window.addEventListener('scroll', () => {
+  const newSection = Math.round(window.scrollY / sizes.height); // 스크롤위치 / 현재 viewport
+
+  if (newSection !== currentSection) {
+    currentSection = newSection;
+
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      x: '+=3',
+      y: '+=5',
+      z: '+=1.5',
+    });
+  }
+});
+
+/**
+ * Cursor
+ */
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener('mousemove', (e) => {
+  cursor.x = (e.clientX / sizes.width) * 0.2 - 0.5; //-0.5 ~ 0.5
+  cursor.y = (e.clientY / sizes.height) * 0.2 - 0.5; //-0.5 ~ 0.5
+});
 
 /**
  * Animate
  */
 const clock = new THREE.Clock();
+let previousTime = 0; // 1 frame별로 걸리는 시간을 정확히 계산하기 위함 (그냥 elapsedTime 사용하면 계속 증가하므로)
 
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
-    // Update controls
-    controls.update();
+  // Camera Animation
+  // const scrollHeight = document.documentElement.scrollHeight;
+  // const maxCamera = objectsDistance * 2;
+  // camera.position.y =
+  //   -maxCamera / ((scrollHeight - window.innerHeight) / window.scrollY); //max: objectDistance*2
+  // 또는...
+  camera.position.y = (-window.scrollY / sizes.height) * objectsDistance;
 
-    // Render
-    renderer.render(scene, camera);
+  // Cursor Animation
+  const parallaxX = cursor.x * 0.5;
+  const parallaxY = -cursor.y * 0.5;
+  cameraGroup.position.x +=
+    (parallaxX - cameraGroup.position.x) * 10 * deltaTime; //cursor:목적지, cameraGroup position: 현재 위치, 0.1: 10%만큼만 이동
+  cameraGroup.position.y +=
+    (parallaxY - cameraGroup.position.y) * 10 * deltaTime;
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick);
+  // Mesh Animation
+  for (const mesh of sectionMeshes) {
+    mesh.rotation.x += deltaTime * 0.1; // gsap 애니메이션에도 적용가능 하도록 frame마다 rotation 플러스
+    mesh.rotation.z += deltaTime * 0.2;
+  }
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
 };
 
 tick();
