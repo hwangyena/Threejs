b94ff2cb6bdff00fc090381edc832db5579b69b6
Hwang yena <center7332@naver.com>
2023-03-26 23:19:37 +0900

    [CH03] 22. Imported models - init

--- ch03/src/script.js
+++ ch03/src/script.js
@@ -1,16 +1,14 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'lil-gui';
-import CANNON from 'cannon';
-
-/**
- * Debug
- */
-const gui = new dat.GUI();
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 /**
  * Base
  */
+// Debug
+const gui = new dat.GUI();
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
@@ -18,127 +16,27 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-
-const environmentMapTexture = cubeTextureLoader.load([
-  '/textures/environmentMaps/0/px.png',
-  '/textures/environmentMaps/0/nx.png',
-  '/textures/environmentMaps/0/py.png',
-  '/textures/environmentMaps/0/ny.png',
-  '/textures/environmentMaps/0/pz.png',
-  '/textures/environmentMaps/0/nz.png',
-]);
-
-/**
- * Physics
- */
-// World
-const world = new CANNON.World();
-world.gravity.set(0, -9.82, 0); //Vec3 classs
-
-// Materials
-const concreteMaterial = new CANNON.Material('concrete'); // params: (name) - 바닥(floorBody)에 부여할 material
-const plasticMaterial = new CANNON.Material('plastic'); // params: (name) - 공(sphere)에 부여할 material
-
-const defaultMaterial = new CANNON.Material('default'); // 만나는 body 모두에 적용해줄 기본 material
-
-//// 방법 1. ContactMaterial - concreate + plastic Material
-// const concreatePlasticContactMaterial = new CANNON.ContactMaterial(
-//   concreteMaterial,
-//   plasticMaterial,
-//   {
-//     friction: 0.1, // 마찰력
-//     restitution: 0.7, //번환(default: 0.3)
-//   }
-// );
-// world.addContactMaterial(concreatePlasticContactMaterial);
-
-//// 방법 2. 동일한 Material 사용하기
-const defaultContactMaterial = new CANNON.ContactMaterial(
-  defaultMaterial,
-  defaultMaterial,
-  {
-    friction: 0.1, // 마찰력
-    restitution: 0.7, //번환(default: 0.3)
-  }
-);
-world.addContactMaterial(defaultContactMaterial);
-
-// 방법3. wordl 전체에 동일한 Material 사용하기
-world.defaultContactMaterial = defaultContactMaterial;
-
-// Sphere
-const sphereShape = new CANNON.Sphere(0.5);
-const sphereBody = new CANNON.Body({
-  mass: 1, // mass가 높은게 더 중량이 크게 작용
-  position: new CANNON.Vec3(0, 3, 0),
-  shape: sphereShape,
-  // material: plasticMaterial, // 방법1
-  // material: defaultMaterial, // 방법2
-});
-
-world.addBody(sphereBody);
-
-// Floor
-const floorShape = new CANNON.Plane(); // infinite plane (이 밖에 있으면 object 관찰 불가능)
-const floorBody = new CANNON.Body({
-  mass: 0, // 움직이지 않는 물체
-  // material: concreteMaterial, // 방법1
-  // material: defaultMaterial, // 방법2
-});
-
-floorBody.addShape(floorShape);
-// floorBody.addShape(floorShape); // Body에 여러개의 shape를 넣을 수 있다.
-floorBody.quaternion.setFromAxisAngle(
-  new CANNON.Vec3(-1, 0, 0), //axios (축) 위치 지정 - 어느 축으로 회전시킬지?
-  Math.PI * 0.5
-); // floorBody 회전
-
-world.addBody(floorBody);
-
-/**
- * Test sphere
- */
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 32, 32),
-  new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture,
-    envMapIntensity: 0.5,
-  })
-);
-sphere.castShadow = true;
-sphere.position.y = 0.5;
-scene.add(sphere);
-
-/**
  * Floor
  */
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(10, 10),
   new THREE.MeshStandardMaterial({
-    color: '#777777',
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture,
-    envMapIntensity: 0.5,
+    color: '#444444',
+    metalness: 0,
+    roughness: 0.5,
   })
 );
 floor.receiveShadow = true;
-floor.rotation.x = -Math.PI * 0.5; //기존 floor는 | 위치에 있다
+floor.rotation.x = -Math.PI * 0.5;
 scene.add(floor);
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize.set(1024, 1024);
 directionalLight.shadow.camera.far = 15;
@@ -181,11 +79,12 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(-3, 3, 3);
+camera.position.set(2, 2, 2);
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 0.75, 0);
 controls.enableDamping = true;
 
 /**
@@ -209,11 +108,6 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
   const deltaTime = elapsedTime - previousTime;
   previousTime = elapsedTime;
-
-  // 물리 적용
-  world.step(1 / 60, deltaTime, 3); // timestamp | delta(이전으로부터 시간 얼마나 지났는지) | 함수 호출당 수행할 최대 고정 단계 수(????)
-
-  sphere.position.copy(sphereBody.position); // Mesh에 물리엔진 적용
 
   // Update controls
   controls.update();
