c18c3434d4845c87c5e26e526bf97452bc469b83
Hwang yena <center7332@naver.com>
2023-03-29 23:06:26 +0900

    [CH03] 25. Realistic render - 햄버거

--- ch03/src/script.js
+++ ch03/src/script.js
@@ -44,6 +44,9 @@ directionalLight.position.set(0.25, 3, -2.25);
 directionalLight.castShadow = true;
 directionalLight.shadow.camera.far = 15;
 directionalLight.shadow.mapSize.set(1024, 1024);
+
+directionalLight.shadow.normalBias = 0.05; //표면을 아래로 밀어냄
+
 scene.add(directionalLight);
 
 const directionalLightCameraHelper = new THREE.CameraHelper(
@@ -65,9 +68,9 @@ gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.01);
  * GLTF
  */
 const gltfLoader = new GLTFLoader();
-gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf) => {
-  gltf.scene.scale.set(10, 10, 10);
-  gltf.scene.position.set(0, -4, 0);
+gltfLoader.load('/models/hamburger.glb', (gltf) => {
+  gltf.scene.scale.set(0.3, 0.3, 0.3);
+  gltf.scene.position.set(0, -1, 0);
   gltf.scene.rotation.y = Math.PI * 0.5;
   scene.add(gltf.scene);
 
