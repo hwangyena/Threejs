e2cf81717cd324485df057528d5864b9d8e45711
Hwang yena <center7332@naver.com>
2023-03-29 22:54:38 +0900

    [CH03] 25. Realistic render - 헬멧

--- ch03/src/script.js
+++ ch03/src/script.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import * as dat from 'lil-gui';
 
 /**
@@ -9,6 +8,7 @@ import * as dat from 'lil-gui';
  */
 // Debug
 const gui = new dat.GUI();
+const debugObject = {};
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -17,51 +17,93 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
- * Models
+ * Materials 업데이트
  */
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath('/draco/');
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    console.log('child', child); // scene에 포함된 모든 obj (camera, Mesh, Light, Group 등 전부 포함)
 
-const gltfLoader = new GLTFLoader();
-gltfLoader.setDRACOLoader(dracoLoader);
-
-let mixer = null;
-
-gltfLoader.load('/models/burger.glb', (gltf) => {
-  scene.add(gltf.scene);
-});
-
-/**
- * Floor
- */
-const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(50, 50),
-  new THREE.MeshStandardMaterial({
-    color: '#444444',
-    metalness: 0,
-    roughness: 0.5,
-  })
-);
-floor.receiveShadow = true;
-floor.rotation.x = -Math.PI * 0.5;
-scene.add(floor);
+    if (
+      child instanceof THREE.Mesh &&
+      child.material instanceof THREE.MeshStandardMaterial
+    ) {
+      // child.material.envMap = environmentMap;    // scene에 바로 추가해줘서 여기서 안써도 됨
+      child.material.envMapIntensity = debugObject.envMapIntensity;
+      child.material.needsUpdate = true;
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+};
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+const directionalLight = new THREE.DirectionalLight('#fff', 4);
+directionalLight.position.set(0.25, 3, -2.25);
 directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
 directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.camera.left = -7;
-directionalLight.shadow.camera.top = 7;
-directionalLight.shadow.camera.right = 7;
-directionalLight.shadow.camera.bottom = -7;
-directionalLight.position.set(5, 5, 5);
+directionalLight.shadow.mapSize.set(1024, 1024);
 scene.add(directionalLight);
+
+const directionalLightCameraHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera
+);
+scene.add(directionalLightCameraHelper);
+
+gui
+  .add(directionalLight, 'intensity')
+  .min(0)
+  .max(10)
+  .step(0.01)
+  .name('lightIntensity');
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.01);
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.01);
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.01);
+
+/**
+ * GLTF
+ */
+const gltfLoader = new GLTFLoader();
+gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf) => {
+  gltf.scene.scale.set(10, 10, 10);
+  gltf.scene.position.set(0, -4, 0);
+  gltf.scene.rotation.y = Math.PI * 0.5;
+  scene.add(gltf.scene);
+
+  gui
+    .add(gltf.scene.rotation, 'y')
+    .min(-Math.PI)
+    .max(Math.PI)
+    .step(0.01)
+    .name('rotation');
+
+  updateAllMaterials(); // gltf까지 모든 obj 업데이트 후 사용
+});
+
+/**
+ * Environment Map
+ */
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const environmentMap = cubeTextureLoader.load([
+  '/textures/environmentMaps/0/px.jpg',
+  '/textures/environmentMaps/0/nx.jpg',
+  '/textures/environmentMaps/0/py.jpg',
+  '/textures/environmentMaps/0/ny.jpg',
+  '/textures/environmentMaps/0/pz.jpg',
+  '/textures/environmentMaps/0/nz.jpg',
+]);
+environmentMap.encoding = THREE.sRGBEncoding;
+scene.background = environmentMap;
+scene.environment = environmentMap; // scene에 environmentMap 더하기
+
+debugObject.envMapIntensity = 5;
+gui
+  .add(debugObject, 'envMapIntensity')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .onChange(updateAllMaterials);
 
 /**
  * Sizes
@@ -95,12 +137,11 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(-8, 4, 8);
+camera.position.set(4, 1, -4);
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
-controls.target.set(0, 1, 0);
 controls.enableDamping = true;
 
 /**
@@ -108,27 +149,32 @@ controls.enableDamping = true;
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  antialias: true, // 계단 현상 방지
 });
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.physicallyCorrectLights = true; // 좀더 현실적인 빛으로 만들어줌
+renderer.outputEncoding = THREE.sRGBEncoding; // 감마인코딩: 출력 렌더링 인코딩 제어(default: LinearEncoding) -> 빛이 더 많이 들어오는 효과
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 3; // 빛 정도
+
+// shadow
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+
+gui.add(renderer, 'toneMapping', {
+  No: THREE.NoToneMapping, //default
+  Linear: THREE.LinearToneMapping,
+  Reinhard: THREE.ReinhardToneMapping,
+  Cineon: THREE.CineonToneMapping,
+  ACESFilmic: THREE.ACESFilmicToneMapping,
+});
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.01);
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
-let previousTime = 0;
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - previousTime;
-  previousTime = elapsedTime;
-
-  if (mixer) {
-    mixer.update(deltaTime);
-  }
-
   // Update controls
   controls.update();
 
