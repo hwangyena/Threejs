ef6a62f8b157e703c98b8d0e720c3bd1833b053f
Hwang yena <center7332@naver.com>
2023-05-01 22:47:16 +0900

    [CH04] 29. Raging sea

--- ch04/src/script.js
+++ ch04/src/script.js
@@ -1,95 +1,196 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
-import testVertexShader from './shaders/test/vertex.glsl'
-import testFragmentShader from './shaders/test/fragment.glsl'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
+import waterVertexShader from './shaders/water/vertex.glsl';
+import waterFragmentShader from './shaders/water/fragment.glsl';
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI({ width: 340 });
+const colorObject = {};
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
- * Test mesh
+ * Water
  */
 // Geometry
-const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128);
+
+// 색상
+colorObject.depthColor = '#146C94';
+colorObject.surfaceColor = '#B0DAFF';
 
 // Material
-const material = new THREE.ShaderMaterial({
-    vertexShader: testVertexShader,
-    fragmentShader: testFragmentShader,
-    side: THREE.DoubleSide
-})
+const waterMaterial = new THREE.ShaderMaterial({
+  vertexShader: waterVertexShader,
+  fragmentShader: waterFragmentShader,
+  uniforms: {
+    uTime: { value: 0.0 }, // wave 애니메이션
+    uSpeed: { value: 0.75 }, // wave 속도
+    uBigWavesElevation: { value: 0.2 }, // wave 높이
+    uBigWavesFrequency: { value: new THREE.Vector2(4, 3) }, // wave 이동 정도 - x,y
+
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 2.0 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallWavesIterations: { value: 4.0 },
+
+    uDepthColor: { value: new THREE.Color(colorObject.depthColor) },
+    uSurfaceColor: { value: new THREE.Color(colorObject.surfaceColor) },
+    uColorOffset: { value: 0.2 },
+    uColorMultiplier: { value: 3 },
+  },
+});
+
+// Debug
+gui
+  .add(waterMaterial.uniforms.uBigWavesElevation, 'value')
+  .min(0)
+  .max(1)
+  .step(0.01)
+  .name('wave 높이');
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x')
+  .min(0)
+  .max(10)
+  .step(0.01)
+  .name('uBigWavesFrequencyX');
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y')
+  .min(0)
+  .max(10)
+  .step(0.01)
+  .name('uBigWavesFrequencyY');
+gui
+  .add(waterMaterial.uniforms.uSpeed, 'value')
+  .min(0)
+  .max(3)
+  .step(0.01)
+  .name('uSpeed');
+
+gui
+  .add(waterMaterial.uniforms.uSmallWavesElevation, 'value')
+  .min(0)
+  .max(1)
+  .step(0.01)
+  .name('uSmallWavesElevation');
+gui
+  .add(waterMaterial.uniforms.uSmallWavesFrequency, 'value')
+  .min(0)
+  .max(30)
+  .step(0.01)
+  .name('uSmallWavesFrequency');
+gui
+  .add(waterMaterial.uniforms.uSmallWavesSpeed, 'value')
+  .min(0)
+  .max(3)
+  .step(0.01)
+  .name('uSmallWavesSpeed');
+gui
+  .add(waterMaterial.uniforms.uSmallWavesIterations, 'value')
+  .min(0)
+  .max(5)
+  .step(0.01)
+  .name('uSmallWavesIterations');
+
+gui.addColor(colorObject, 'depthColor').onChange(() => {
+  waterMaterial.uniforms.uDepthColor.value.set(colorObject.depthColor);
+});
+gui.addColor(colorObject, 'surfaceColor').onChange(() => {
+  waterMaterial.uniforms.uSurfaceColor.value.set(colorObject.surfaceColor);
+});
+gui
+  .add(waterMaterial.uniforms.uColorOffset, 'value')
+  .min(0)
+  .max(1)
+  .step(0.01)
+  .name('uColorOffset');
+gui
+  .add(waterMaterial.uniforms.uColorMultiplier, 'value')
+  .min(0)
+  .max(10)
+  .step(0.01)
+  .name('uColorMultiplier');
 
 // Mesh
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const water = new THREE.Mesh(waterGeometry, waterMaterial);
+water.rotation.x = -Math.PI * 0.5;
+scene.add(water);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0.25, - 0.25, 1)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(1, 1, 1);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
+const clock = new THREE.Clock();
 
-    // Render
-    renderer.render(scene, camera)
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // 물결 애니메이션
+  waterMaterial.uniforms.uTime.value = elapsedTime;
 
-tick()
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
